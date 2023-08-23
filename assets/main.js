0dfa9b129f7113ca26222456ac5cdaba66363874
chenqinyang <chenqinyang@cmcm.com>
2023-08-23 12:40:56 +0800

    feat: 完善功能

--- assets/main.js
+++ assets/main.js
@@ -4,15 +4,26 @@ $("#paihaoxinxi").click(function () {
 
 const form = document.getElementById("myForm");
 
+$("#formClose").click(function () {
+  $("#myformwrap").hide();
+});
+
 form.addEventListener("submit", function (event) {
   event.preventDefault(); // 阻止默认的表单提交行为
 
   const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const age = document.getElementById("age").value;
+  const number = document.getElementById("number").value;
+  const renshu = document.getElementById("renshu").value;
+  const zhuoxing = document.getElementById("zhuoxing").value;
+  const shibie = document.getElementById("shibie").value;
+  const curNumber = document.getElementById("curNumber").value;
 
   $("#storeName").text(name);
-  $("#queueNum").text(`${email}号`);
-  $("#wait").text(`当前叫号：小桌${age}号`);
+  $("#queueNum").text(`${number}号`);
+  $("#ParticipantNumber").text(`${renshu}人`);
+  $("#tableType").text(zhuoxing);
+  $("#timeType").text(shibie);
+  $("#wait").text(`当前叫号：小桌${curNumber}号`);
+
   $("#myformwrap").hide();
 });
